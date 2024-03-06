c5321ba71861e4ea4e257f2c5b6faac3b8209883
Ayaal Ivanov <ivanov_ai2@albank.ru>
2024-03-06 12:26:58 +0900

    Initial Commit

---
+++ VerticalCustomList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VerticalCustomList
+//
+//  Created by Ayaal Ivanov on 06.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VerticalCustomList/VerticalCustomListApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerticalCustomListApp.swift
+//  VerticalCustomList
+//
+//  Created by Ayaal Ivanov on 06.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct VerticalCustomListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
